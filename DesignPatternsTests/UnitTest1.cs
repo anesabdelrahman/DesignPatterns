bc4a2cac62e88b1056e4c3f351e1720c14420ad4
Anes Abdelrahman <anes.abdelrahman@accesspay.com>
2023-05-16 07:35:30 +0100

    Initial commit

---
+++ DesignPatternsTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace DesignPatternsTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
