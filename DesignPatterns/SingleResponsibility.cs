4b84aed5e61a5b1c35936687ca720008a87166b9
Anes Abdelrahman <anes.abdelrahman@accesspay.com>
2023-05-16 08:22:48 +0100

    Single Responsibility Pattern

---
+++ DesignPatterns/SingleResponsibility.cs
@@ -0,0 +1,46 @@
+﻿namespace DesignPatterns
+{
+    public class SingleResponsibility
+    {
+        public Journal Journal;
+        public JournalRepository JournalRepository;
+        public SingleResponsibility()
+        {
+            Journal = new Journal();
+            JournalRepository = new JournalRepository();
+        }
+    }
+
+    public class Journal
+    {
+        private readonly List<string> _journals = new();
+        public int Count = 0;
+
+        public int AddJournal(string journal)
+        {
+            _journals.Add($"{++Count}: {journal}");
+            return Count;
+        }
+
+        public void DeleteJournal(int index)
+        {
+            _journals.RemoveAt(index);
+            Count--;
+        }
+
+        public override string ToString()
+        {
+            return string.Join(Environment.NewLine, _journals);
+        }
+    }
+
+    public class JournalRepository {
+        public static void PersistJournal(Journal journal, string fileName, bool overwrite = false)
+        {
+            if (overwrite || !File.Exists(fileName))
+            {
+                File.WriteAllText(fileName, journal.ToString());
+            }
+        }
+    }
+}
---
+++ DesignPatternsTests/SingleResponsibilityTest.cs
@@ -0,0 +1,41 @@
+﻿using DesignPatterns;
+
+namespace DesignPatternsTests
+{
+    public class Tests
+    {
+        [Test]
+        public void Test_AddingJournal_CountShouldIncrement()
+        {
+            var newJournal = "I am happy";
+            var sut = new SingleResponsibility();
+            var result = sut.Journal.AddJournal(newJournal);
+           Assert.That(result, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Test_AddingMultipleJournals_CountShouldIncrementAccordingly()
+        {
+            var sut = new SingleResponsibility();
+            var newJournals = new [] {"I am happy", "I am awesome", "I worked out today!"};
+            
+            foreach (var newJournal in newJournals)  sut.Journal.AddJournal(newJournal);
+
+
+            Assert.That(sut.Journal.Count, Is.EqualTo(newJournals.Length));
+        }
+
+        [Test]
+        public void Test_RemovingSingleJournal_CountShouldDecrementAccordingly()
+        {
+            var sut = new SingleResponsibility();
+            var newJournals = new[] { "I am happy", "I am awesome", "I worked out today!" };
+            
+            foreach (var newJournal in newJournals) sut.Journal.AddJournal(newJournal);
+
+            sut.Journal.DeleteJournal(1);
+           
+            Assert.That(sut.Journal.Count, Is.EqualTo(newJournals.Length-1));
+        }
+    }
+}
